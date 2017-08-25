b5678e7bd66edea5c456076fb744bfab55d01576
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-08-25 15:28:24 -0700

    fix errors thrown upon submit event, will now work on resetting input fields upon click event

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //Backend Logic
-function Pizza(size, toppings) {
+function Pizza(size, toppings, price) {
   this.size = size;
   this.toppings = toppings;
   this.price = price;
@@ -7,46 +7,55 @@ function Pizza(size, toppings) {
 
 Pizza.prototype.cost = function() {
   if (this.size === "small") {
-    this.price === 10;
+    this.price = 10;
   } else if (this.size === "medium") {
-    this.price === 15;
+    this.price = 15;
   } else if (this.size === "large") {
-    this.price === 20;
+    this.price = 20;
   }
+  console.log(this.price);
 
   if (this.toppings.length === 0) {
-    var finalPrice === this.price;
+    var finalPrice = this.price;
   } else if (this.toppings.length === 1) {
-    var finalPrice === this.price + 1;
+    var finalPrice = this.price + 1;
   } else if (this.toppings.length === 2) {
-    var finalPrice === this.price + 2;
+    var finalPrice = this.price + 2;
   } else if (this.toppings.length === 3) {
-    var finalPrice === this.price + 3;
+    var finalPrice = this.price + 3;
   } else if (this.toppings.length === 4) {
-    var finalPrice === this.price + 4;
+    var finalPrice = this.price + 4;
   } else if (this.toppings.length === 5) {
-    var finalPrice === this.price + 5;
+    var finalPrice = this.price + 5;
   } else if (this.toppings.length === 6) {
-    var finalPrice === this.price + 6;
+    var finalPrice = this.price + 6;
   } else if (this.toppings.length === 7) {
-    var finalPrice === this.price + 7;
+    var finalPrice = this.price + 7;
   }
   return finalPrice;
-  alert(finalPrice);
 }
 
-var pizzaPrice = new Pizza(pizza-size, pizza-toppings);
+var pizzaPrice = 0;
+
+function resetFields() {
+
+}
 
 //Frontend Logic
 $(document).ready(function(){
-  $("form#pizzaForm").submit(function(event){
+  $("form#pizzaForm").submit(function(event) {
     event.preventDefault();
 
     var pizzaToppings = [];
-    var pizzaSize = $("input[name=pizza-size]: checked").val();
-    $("input:checkbox[name=pizza-toppings]:checked").each(function) {
-      var pizzaToppings = $(this).val();
-    }
+    var pizzaSize = $("input[name='pizza-size']:checked").val();
+    $("input:checkbox[name='pizza-toppings']:checked").each(function() {
+      pizzaToppings.push($(this).val());
+    });
+    pizza = new Pizza(pizzaSize, pizzaToppings);
+    alert("Your delicious pizza will cost: $" + pizza.cost());
+
+    setTimeout(function () {
+      resetFields();
+    }, 10);
   });
-  alert(pizzaPice);
 });
